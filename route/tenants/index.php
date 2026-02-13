60f7e57bd80d15050cd4ae4d30641384dd410de6
Miha Petkovšek <miha.petkovsek@telemach.si>
2026-02-13 13:07:35 +0100

    Validation for tenants

--- route/tenants/index.php
+++ route/tenants/index.php
@@ -64,8 +64,27 @@ else {
 	$users  = $Database->count_database_objects("users", "t_id", $t->id);
 	$agents = $Database->count_database_objects("agents", "t_id", $t->id);
 
+	# check for missing cronjobs and portgroups
+	$warnings = [];
+	$check_cronjobs = $Database->getObjectsQuery("select count(*) as cnt from cron where t_id = ?", [$t->id]);
+	if($check_cronjobs[0]->cnt == 0) {
+		$warnings[] = _("No cronjobs configured");
+	}
+	$check_portgroups = $Database->getObjectsQuery("select count(*) as cnt from ssl_port_groups where t_id = ?", [$t->id]);
+	if($check_portgroups[0]->cnt == 0) {
+		$warnings[] = _("No port groups configured");
+	}
+
+	# warning icon and popup
+	$warning_icon = "";
+	$tenant_name_class = "text-body";
+	if(sizeof($warnings)>0) {
+		$warning_icon = " <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='icon icon-tabler icons-tabler-outline icon-tabler-alert-triangle text-red' data-bs-toggle='tooltip' data-bs-html='true' title=\"".implode("<br>", $warnings)."\" data-bs-placement='right'><path stroke='none' d='M0 0h24v24H0z' fill='none'/><path d='M12 9v4' /><path d='M10.363 3.591l-8.106 13.534a1.914 1.914 0 0 0 1.636 2.871h16.214a1.914 1.914 0 0 0 1.636 -2.87l-8.106 -13.536a1.914 1.914 0 0 0 -3.274 0' /><path d='M12 16h.01' /></svg>";
+		$tenant_name_class = "text-red";
+	}
+
 	print "<tr>";
-	print "	<td>".$url_items["tenants"]['icon']." <a href='/route/modals/tenants/edit.php?id=".$t->id."&action=edit' data-bs-toggle='modal' data-bs-target='#modal1' style='color:var(--tblr-primary-color);'>".$t->name."</a></td>";
+	print "	<td>".$url_items["tenants"]['icon']." <a href='/route/modals/tenants/edit.php?id=".$t->id."&action=edit' data-bs-toggle='modal' data-bs-target='#modal1' class='".$tenant_name_class."'>".$t->name."</a>".$warning_icon."</td>";
 	print "	<td><span class='badge' style='width:100%'>".$t->id."</span></td>";
 	print "	<td>".$status."</td>";
 	print "	<td class='text-muted'>".$t->description."</td>";
