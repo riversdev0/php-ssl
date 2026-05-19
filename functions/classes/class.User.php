dc0e41f114c0b5eb4755912bf72d7b8c5c67f480
riversdev0 <riversdev0@outlook.com>
2026-05-19 08:07:16 -0500

    string-to-int comparison failure The database column is int and all the code is using strings.

--- functions/classes/class.User.php
+++ functions/classes/class.User.php
@@ -164,6 +164,7 @@ class User extends Common
 				$this->errors[] = $e->getMessage();
 				$this->result_die();
 			}
+			$user->admin = strval($user->admin);
 			// save
 			if ($user != null) {
 				$this->user = $user;
@@ -429,7 +430,7 @@ class User extends Common
 			}
 		}
 		// not admin
-		elseif ($require_admin && $this->user->admin != 1) {
+		elseif ($require_admin && $this->user->admin != "1") {
 			if ($is_popup && !$is_popup_result) {
 				global $Modal;
 				$Modal->modal_print("Error", "<div class='alert alert-danger'>" . _("Administrative privileges required") . ".</div>", "", false, "danger");
